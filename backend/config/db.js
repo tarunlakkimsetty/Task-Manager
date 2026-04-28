9f0ae3061e604802e7db121d290d9fa994dd7be4
tarunlakkimsetty <tarunlakkimsetty@gmail.com>
2026-04-28 16:32:58 +0530

    fix: update DB config to use DATABASE_URL for Railway

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,16 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 module.exports = sequelize;
